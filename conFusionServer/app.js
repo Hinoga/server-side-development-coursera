b58d80d2fd177fd06a753a2fe7f47bf74db1f99e
Hinoga <oscargallego27@gmail.com>
2020-09-27 15:39:45 -0500

    Assignment 3

--- conFusionServer/app.js
+++ conFusionServer/app.js
@@ -1,17 +1,17 @@
-var createError = require('http-errors')
-var express = require('express')
-var path = require('path')
-var logger = require('morgan')
-var passport = require('passport')
+const createError = require('http-errors')
+const express = require('express')
+const path = require('path')
+const logger = require('morgan')
+const passport = require('passport')
 const mongoose = require('mongoose')
 
-var config = require('./config')
+const config = require('./config')
 
-var indexRouter = require('./routes/index')
-var usersRouter = require('./routes/users')
-var dishRouter = require('./routes/dishRouter')
-var promoRouter = require('./routes/promoRouter')
-var leaderRouter = require('./routes/leaderRouter')
+const indexRouter = require('./routes/index')
+const usersRouter = require('./routes/users')
+const dishRouter = require('./routes/dishRouter')
+const promoRouter = require('./routes/promoRouter')
+const leaderRouter = require('./routes/leaderRouter')
 
 const url = config.mongoUrl
 const connect = mongoose.connect(url)
@@ -25,7 +25,7 @@ connect.then(
   }
 )
 
-var app = express()
+const app = express()
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'))
--- conFusionServer/authenticate.js
+++ conFusionServer/authenticate.js
@@ -1,11 +1,12 @@
-var passport = require('passport')
-var LocalStrategy = require('passport-local').Strategy
-var User = require('./models/user')
-var JwtStrategy = require('passport-jwt').Strategy
-var ExtractJwt = require('passport-jwt').ExtractJwt
-var jwt = require('jsonwebtoken')
+const createError = require('http-errors')
+const passport = require('passport')
+const LocalStrategy = require('passport-local').Strategy
+const User = require('./models/user')
+const JwtStrategy = require('passport-jwt').Strategy
+const ExtractJwt = require('passport-jwt').ExtractJwt
+const jwt = require('jsonwebtoken')
 
-var config = require('./config.js')
+const config = require('./config.js')
 
 passport.use(new LocalStrategy(User.authenticate()))
 passport.serializeUser(User.serializeUser())
@@ -15,7 +16,7 @@ exports.getToken = function(user) {
   return jwt.sign(user, config.secretKey, { expiresIn: 3600 })
 }
 
-var opts = {}
+const opts = {}
 opts.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken()
 opts.secretOrKey = config.secretKey
 
@@ -35,3 +36,12 @@ exports.jwtPassport = passport.use(
 )
 
 exports.verifyUser = passport.authenticate('jwt', { session: false })
+
+exports.verifyAdmin = function(req, res, next) {
+  if (!req.user.admin) {
+    err = new Error('You are not authorized to perform this operation!')
+    err.status = 403
+    return next(err)
+  }
+  next()
+}
